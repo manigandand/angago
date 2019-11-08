57670f950ccab7af6d634a4834d7ccd1d0b87d69
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2019-11-08 20:53:24 +0530

    respond not found if the host is not in the configuration

--- angago.go
+++ angago.go
@@ -14,8 +14,8 @@ func angagoServer(w http.ResponseWriter, r *http.Request) {
 		res := map[string]interface{}{
 			"host": r.Host,
 			"meta": map[string]interface{}{
-				"status":      http.StatusText(http.StatusBadRequest),
-				"status_code": http.StatusBadRequest,
+				"status":      http.StatusText(http.StatusNotFound),
+				"status_code": http.StatusNotFound,
 				"error":       err.Error(),
 			},
 		}
@@ -25,7 +25,7 @@ func angagoServer(w http.ResponseWriter, r *http.Request) {
 			log.Println("error:", err.Error())
 			return
 		}
-
+		w.WriteHeader(http.StatusNotFound)
 		// w.Header().Set("Host", r.Host)
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(resp)
